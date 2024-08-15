6ef416ec94eb65e31d16d099888305103da2d71a
a-levitt <artemis.levitt@gmail.com>
2024-08-15 18:25:22 +0200

    Set up testNG and driver initialization

---
+++ src/test/java/qa/mobile/TestApp.java
@@ -0,0 +1,20 @@
+package qa.mobile;
+
+import org.testng.annotations.*;
+
+public class TestApp {
+    @Test
+    public void f() {
+
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+    }
+}
